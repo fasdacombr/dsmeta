71017a74290c6feb2dfbe15ee2ebbc41f2ee0dd3
Flavio <flavio_125@hotmail.com>
2022-07-13 19:59:28 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá Flávio!</h1>
+  return (
+    <>
+      <h1>Olá Flávio!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
